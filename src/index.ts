d4a09fa8d7ea892f4bca043974190d7150d33035
VlaDinter <vladyslavdinter@gmail.com>
2023-10-26 17:13:54 +0300

    Fix4

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { app } from './app';
-import { Server } from 'http';
 
 const port = process.env.PORT || 3999;
 
-const server: Server = app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
 
