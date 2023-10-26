553bf9718175246b70c7953fa523a8ed26386ea6
VlaDinter <vladyslavdinter@gmail.com>
2023-10-26 17:09:57 +0300

    Fix3

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express from 'express';
-import { getAppRouter } from './settings';
+import express, {Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import { routerPaths } from './utils';
 import { videosRouter } from './routes/videos-router';
+import {videosLocalRepository} from "./repositories/videos-repository";
+import {CodeResponsesEnum} from "./types";
 // import { blogsRouter } from './routes/blogs-router';
 // import { postsRouter } from './routes/posts-router';
 
@@ -12,4 +13,13 @@ app.use(bodyParser({}));
 app.use(routerPaths.videos, videosRouter);
 // app.use(routerPaths.blogs, blogsRouter);
 // app.use(routerPaths.posts, postsRouter);
-getAppRouter(app);
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello back-end HomeWorks in it-incubator!!!');
+});
+
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+    videosLocalRepository.deleteAll();
+    // blogsLocalRepository.deleteAll();
+    // postsLocalRepository.deleteAll();
+    res.sendStatus(CodeResponsesEnum.Not_content_204);
+});
